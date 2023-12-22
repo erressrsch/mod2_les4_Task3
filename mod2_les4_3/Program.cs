317223649b168ff2be3f8f24a16ba8c144014460
Szymon Chyc <erressrsch@gmail.com>
2023-12-22 21:19:09 +0100

    Add project files.

---
+++ mod2_les4_3/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace modul2_les4_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static double diagonal(double width = 0, double length = 0)
+            {
+                int number = 2;
+
+                Console.WriteLine("Enter width: ");
+                width = double.Parse(Console.ReadLine());
+                width = Math.Pow(width, number);
+
+                Console.WriteLine("Enter length: ");
+                length = double.Parse(Console.ReadLine());
+                length = Math.Pow(length, number);
+
+                return Math.Sqrt(width + length);
+
+
+            }
+            Console.WriteLine(diagonal());
+        }
+    }
+}
